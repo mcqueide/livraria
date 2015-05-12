2d66d6ad00976b18bda4768932b3293f822755a5
john.clemente <john.clemente@johnclemente.foton.local>
2015-05-12 09:44:50 -0300

    Fim da aula 5

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -60,6 +62,13 @@ public class LivroBean implements Serializable {
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
 
 		this.livro = new Livro();
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+	    String valor = value.toString();
+	    if (!valor.startsWith("1")) {
+	        throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+	    }
 	}
 
 }
